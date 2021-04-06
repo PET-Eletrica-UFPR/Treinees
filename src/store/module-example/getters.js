95b3f05875ff77e96d37e71821b14e6b72061b1d
jp-turra <63259730+jp-turra@users.noreply.github.com>
2021-04-06 20:47:55 -0300

    implementação tabs com axios/vuex

--- src/store/module-example/getters.js
+++ src/store/module-example/getters.js
@@ -1,2 +1,3 @@
-export function someGetter (/* state */) {
+export function getTab (state) {
+  return state.tab
 }
--- src/store/module-example/mutations.js
+++ src/store/module-example/mutations.js
@@ -1,2 +1,3 @@
-export function someMutation (/* state */) {
+export function setTab (state, tab) {
+  state.tab = tab
 }
