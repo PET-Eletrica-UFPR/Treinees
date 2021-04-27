4c273546b30d2e8e6035ccec2a6b8be9bb40da6f
Leonardo Santos <leonard.andrade@ufpr.br>
2021-04-27 11:56:05 -0300

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/myLayout.vue'),
     children: [
-      { path: '', component: () => import('src/pages/Tabs.vue') }
+      { path: '', component: () => import('pages/Tabs.vue') }
     ]
   },
 
